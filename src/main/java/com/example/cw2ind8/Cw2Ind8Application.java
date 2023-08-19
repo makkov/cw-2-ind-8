1f552f370879f3f1b9cc81425d597a90d948093e
maks <maxim_86nva@mail.ru>
2023-08-19 09:43:14 +0300

    init project

---
+++ src/main/java/com/example/cw2ind8/Cw2Ind8Application.java
@@ -0,0 +1,13 @@
+package com.example.cw2ind8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cw2Ind8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cw2Ind8Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cw2ind8/Cw2Ind8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cw2ind8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cw2Ind8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
